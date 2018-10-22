90f6b76ba2ad442cec313a9cd5a4de0dc4301269
denisakira <denis_akira@hotmail.com>
2018-10-22 12:30:03 -0300

    initial commit

---
+++ src/app/enfrentamento/enfrentamento.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enfrentamento',
+  templateUrl: './enfrentamento.page.html',
+  styleUrls: ['./enfrentamento.page.scss'],
+})
+export class EnfrentamentoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
