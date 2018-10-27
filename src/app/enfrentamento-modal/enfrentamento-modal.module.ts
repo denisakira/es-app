cd43c2ec552e3cb62fb8dc129d562cca2b85d83f
denisakira <denis_akira@hotmail.com>
2018-10-27 00:48:13 -0300

    adicionando firebase

--- src/app/enfrentamento-modal/enfrentamento-modal.module.ts
+++ src/app/enfrentamento-modal/enfrentamento-modal.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { EnfrentamentoModalPage } from './enfrentamento-modal.page';
+import { UserService } from '../services/user.service';
 
 const routes: Routes = [
   {
@@ -21,6 +22,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
+  providers: [UserService],
   declarations: [EnfrentamentoModalPage]
 })
 export class EnfrentamentoModalPageModule {}
--- src/app/enfrentamento-modal/enfrentamento-modal.page.ts
+++ src/app/enfrentamento-modal/enfrentamento-modal.page.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { UserService, User } from "../services/user.service";
 
 @Component({
-  selector: 'app-enfrentamento-modal',
-  templateUrl: './enfrentamento-modal.page.html',
-  styleUrls: ['./enfrentamento-modal.page.scss'],
+  selector: "app-enfrentamento-modal",
+  templateUrl: "./enfrentamento-modal.page.html",
+  styleUrls: ["./enfrentamento-modal.page.scss"]
 })
 export class EnfrentamentoModalPage implements OnInit {
+  users: User[];
 
-  constructor(public modalController: ModalController) { }
+  constructor(
+    public modalController: ModalController,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
+    this.userService.getUsers().subscribe(res => {
+      this.users = res;
+    });
   }
 
   closeModal() {
     this.modalController.dismiss();
   }
-
 }
