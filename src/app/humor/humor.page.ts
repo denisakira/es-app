c35c38490322048f20ae6e96208e51a1dcccd083
joao vitor <94joaovitorls@gmail.com>
2018-11-19 22:21:51 -0200

    form humor criado

--- src/app/humor/humor.page.ts
+++ src/app/humor/humor.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-humor',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./humor.page.scss'],
 })
 export class HumorPage implements OnInit {
+  formHumor: FormGroup;
+  humores: Observable<any>;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private humorService: HumorService) { }
 
   ngOnInit() {
+    // Definição do formulario.
+    this.formHumor = this.formBuilder.group({
+      humor: ['', Validators.required],
+    });
+    // Carregamento dos registros salvos.
+    this.carregaHumores();
+  }
+
+  // Salva um novo registro no firebase.
+  submitNovoHumor() {
+  }
+
+  // Carrega os registros existentes no firebase.
+  carregaHumores() {
+    this.humores = this.humorService.carregaHumores();
   }
 
 }
